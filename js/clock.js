d47195b28c3f0d6fa31933d5db56e2ddf9bfc4ef
hhyukk <hhyukk1273@gmail.com>
2023-07-05 21:26:35 +0900

    Timeouts and Dates
    setInterval(sayHello, 1000);
    sayHello() 라는 function을 1초마다 반복한다는 의미.
    단 바로 실행되지 않고 1초 후 첫 시작이 되고 계속 1초마다 반복된다.
    
    setTimeout(sayHello, 1000);
    1초 기다렸다가 한번만 실행.

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,10 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
 // 실행하고자 하는 function과 ms(milliseconds: 1000ms -> 1초)를 argument로 받는다.
