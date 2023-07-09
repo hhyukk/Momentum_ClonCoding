3688a0ee87ffee9a0664c41574a744a07406970d
hhyukk <hhyukk1273@gmail.com>
2023-07-09 17:44:52 +0900

    PadStart
    어떤 문제에 부딪힐 때마다 다른 개발자들도 이미 똑같은 문제를 100% 겪었기 때문에 이미 그 작업을 수행하는 function이 있을 것이다.
    
    padStart(2, 0)는 string의 길이를 2로 만드는데 string의 길이가 2가 아니라면 앞쪽에 0 채우는 함수이다.
    
    padEnd()도 같은 function이지만 뒤쪽에 문자를 추가한다.

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,15 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  // padStart(2, 0)는 string의 길이를 2로 만드는데 string의 길이가 2가 아니라면 앞쪽에 0 채우는 함수이다
+  // date.get으로 얻은 시간들은 number이기 때문에 String으로 변환해야 padStart()를 사용할 수 있다.
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
 setInterval(getClock, 1000);
 // 실행하고자 하는 function과 ms(milliseconds: 1000ms -> 1초)를 argument로 받는다.
+
