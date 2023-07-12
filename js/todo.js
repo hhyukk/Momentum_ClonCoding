13dd1810cae00c41cec965f54b83968fa8775fcf
hhyukk <hhyukk1273@gmail.com>
2023-07-12 11:55:05 +0900

    To Do List - Deleting To Dos
     const li = event.target.parentElement;
    // 이벤트가 발생했들때.해당객체를지정하여, 그것의부모태그를지정;
    
    function deleteTodo() {
    const li = this.parentElement
    li.remove()
    }이렇게 해도 똑같이 작동한다.
    ->this 키워드는 어느 언어에서든 자기 자신을 뜻하기 때문에 버튼 클릭 이벤트 시 자신의 부모 요소를 가리키게 됨

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,23 @@ const toDoInput = toDoForm.querySelector("input");
 // const ToDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  // target은 클릭된 HTML element이다.
+  // parentElement는 클릭된 element의 부모이다.
+  // 삭제하고 싶은 list가 저장
+  li.remove();
+  // list 제거
+}
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
   console.log(li);
 }
